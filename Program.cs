f4d1bc2f8bc647fcae0355520d970e3e7789c691
ADK <artemdk7@gmail.com>
2022-10-26 16:39:21 +0700

    создание нового массива из заданного

--- Program.cs
+++ Program.cs
@@ -9,3 +9,20 @@ for (int i = 0; i < n; i++)
     massiv[i] = Console.ReadLine(); 
     if (massiv[i].Length <= 3) size++; 
 } 
+string[] createMass = NewMassCreate(massiv, size); 
+NewMasPrint(massiv, createMass); 
+ 
+string[] NewMassCreate(string[] array, int a) 
+{ 
+    int sizenewmas = 0; 
+    string[] newmas = new string[a]; 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        if (array[i].Length <= 3) 
+        { 
+            newmas[sizenewmas] = array[i]; 
+            sizenewmas++; 
+        } 
+    } 
+    return newmas; 
+} 
