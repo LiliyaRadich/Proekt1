05e9ebf90d496106dcbbac58d0be016d36e70acb
ADK <artemdk7@gmail.com>
2022-10-26 16:38:21 +0700

    массив задан

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear(); 
+Console.Write("Задайте количество строк, которые вы хотите ввести: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+int size = 0; 
+string[] massiv = new string[n]; 
+for (int i = 0; i < n; i++) 
+{ 
+    Console.Write("Введите строку №{0}: ", i + 1); 
+    massiv[i] = Console.ReadLine(); 
+    if (massiv[i].Length <= 3) size++; 
+} 
